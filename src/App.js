3e10349d6ad3a8ba8ff9d87405f174d133e05e53
louiseeasterbrook <louise.easterbrook@gmail.com>
2021-09-17 14:57:24 +1000

    add routing and product fetching

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+//screens
 import HomeScreen from "./screens/HomeScreen";
+import CartScreen from "./screens/CartScreen";
+import ProductScreen from "./screens/ProductScreen";
+
+//components
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  //get products data
+  useEffect(() => {
+    axios.get("/api/products").then((response) => {
+      console.log(response.data);
+      setProducts(response.data);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <HomeScreen />
+      <Router>
+        <NavBar />
+        <main>
+          <Switch>
+            <Route exact path="/">
+              <HomeScreen products={products} />
+            </Route>
+            <Route exact path="/cart">
+              <CartScreen />
+            </Route>
+            <Route exact path="/product">
+              <ProductScreen />
+            </Route>
+          </Switch>
+        </main>
+        <Footer />
+      </Router>
     </div>
   );
 }
