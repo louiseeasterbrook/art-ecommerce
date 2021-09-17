b6c68602fa24705b6dc6d362b8624b1f2d794f39
louiseeasterbrook <louise.easterbrook@gmail.com>
2021-09-17 14:58:49 +1000

    add product and cart screen

---
+++ src/screens/CartScreen.js
@@ -0,0 +1,13 @@
+import "./CartScreen.css";
+
+const CartScreen = () => {
+  return (
+    <div>
+      <div>
+        <h1>Cart</h1>
+      </div>
+    </div>
+  );
+};
+
+export default CartScreen;
---
+++ src/screens/ProductScreen.js
@@ -0,0 +1,13 @@
+import "./ProductScreen.css";
+
+const ProductScreen = () => {
+  return (
+    <div>
+      <div>
+        <h1>Product screen</h1>
+      </div>
+    </div>
+  );
+};
+
+export default ProductScreen;
