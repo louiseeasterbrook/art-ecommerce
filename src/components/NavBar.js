206ccff1b2b61f67d479e1c668cb5fa646201c1f
louiseeasterbrook <louise.easterbrook@gmail.com>
2021-09-17 14:57:58 +1000

    add active links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,18 @@
 import "./NavBar.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <div className="nav-outer">
       <div className="nav-inner">
-        <h1 className="pageTitle">Art by Carla</h1>
-        <FontAwesomeIcon icon={faShoppingCart} className="shop-icon icon" />
+        <Link className="link" to={"/"}>
+          <h1 className="pageTitle">Art by Carla</h1>
+        </Link>
+        <Link to={"/cart"}>
+          <FontAwesomeIcon icon={faShoppingCart} className="shop-icon icon" />
+        </Link>
       </div>
     </div>
   );
