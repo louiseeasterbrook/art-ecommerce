25c11461947c5d7f25b1c299064eb009cc715df3
louiseeasterbrook <louise.easterbrook@gmail.com>
2021-09-23 00:14:23 +1000

    change page heading

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,9 +7,12 @@ const NavBar = () => {
   return (
     <div className="nav-outer">
       <div className="nav-inner">
-        <Link className="link" to={"/"}>
-          <h1 className="pageTitle">Art by Carla</h1>
-        </Link>
+        <div className="title-container">
+          <Link className="link" to={"/"}>
+            <h1 className="pageTitle">Carla & Fig Art</h1>
+          </Link>
+          <p className="nav-blurb">Homemade ceramic creations</p>
+        </div>
         <Link to={"/cart"}>
           <FontAwesomeIcon icon={faShoppingCart} className="shop-icon icon" />
         </Link>
