4060ceffc6a1fce5ce4eb283dd5443129bfb8389
louiseeasterbrook <louise.easterbrook@gmail.com>
2021-09-22 11:47:53 +1000

    get products from redux state

--- src/sections/Section1.js
+++ src/sections/Section1.js
@@ -1,15 +1,41 @@
 import "./Section1.css";
 import ProductCard from "../components/ProductCard";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
-const Section1 = ({ products }) => {
-  console.log(products);
+//actions
+import { getProducts as listProducts } from "../redux/actions/productActions";
+
+const Section1 = () => {
+  const dispatch = useDispatch();
+
+  const getProducts = useSelector((state) => state.getProducts);
+
+  const { products, loading, error } = getProducts;
+
+  useEffect(() => {
+    dispatch(listProducts());
+  }, [dispatch]);
+
   return (
     <div className="s1-container">
       <h2 className="s1-title">Products</h2>
       <div className="product-container">
-        {products.map((el, i) => (
-          <ProductCard key={i} image={el.image} name={el.name} price="59.99" />
-        ))}
+        {loading ? (
+          <h2>Loading..</h2>
+        ) : error ? (
+          <h2>{error}</h2>
+        ) : (
+          products.map((el) => (
+            <ProductCard
+              key={el._id}
+              image={el.image}
+              name={el.name}
+              price={el.price}
+              id={el._id}
+            />
+          ))
+        )}
       </div>
     </div>
   );
