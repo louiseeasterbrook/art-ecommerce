92a7f144d95bc3088061decaad27110fbfc8d534
louiseeasterbrook <louise.easterbrook@gmail.com>
2021-09-22 11:54:09 +1000

    add link and extra prop to product card

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,12 +1,17 @@
 import "./ProductCard.css";
+import { Link } from "react-router-dom";
 
-const ProductCard = ({ image, name, price }) => {
+const ProductCard = ({ image, name, price, id }) => {
   return (
-    <div className="card-container">
-      <img src={image} alt="product" />
-      <h3>{name}</h3>
-      <p>${price}</p>
-    </div>
+    <Link className="link" to={`/product/${id}`}>
+      <div className="card-container">
+        <div class="displayimg-holder">
+          <img className="card-img" src={image} alt="product" />
+        </div>
+        <h3>{name}</h3>
+        <p>${price}</p>
+      </div>
+    </Link>
   );
 };
 
