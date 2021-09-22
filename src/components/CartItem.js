d5e97c21e3fee0881b3197dc3e81f159a85eac1f
louiseeasterbrook <louise.easterbrook@gmail.com>
2021-09-22 11:52:51 +1000

    create cart item component

---
+++ src/components/CartItem.js
@@ -0,0 +1,36 @@
+import "./CartItem.css";
+// import { useState } from "react";
+
+const CartItem = ({ item, qtyChange, removeItem }) => {
+  const itemTotalPrice = () => {
+    return item.price * item.qty;
+  };
+
+  return (
+    <div className="cartItem-outer">
+      <div className="cartItem-inner">
+        <div className="cart-imgholder">
+          <img className="cart-img" src={item.image} alt={item.name} />
+        </div>
+        <div className="name-holder">
+          <p className="cart-name">{item.name}</p>
+          <p className="cart-remove" onClick={() => removeItem(item.id)}>
+            remove
+          </p>
+        </div>
+        <p className="cart-price">${item.price}</p>
+        <div className="cart-qty">
+          <input
+            type="Number"
+            value={item.qty}
+            //update quantity in state
+            onChange={(e) => qtyChange(item.id, e.target.value)}
+          />
+        </div>
+        <p className="cart-total">${itemTotalPrice().toFixed(2)}</p>
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
