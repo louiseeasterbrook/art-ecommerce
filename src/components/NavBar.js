2d23c1bca6b9d9007aa7ab42141da768b516fcc9
louiseeasterbrook <louise.easterbrook@gmail.com>
2021-09-23 16:49:33 +1000

    add cart counter to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,13 @@ import "./NavBar.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const NavBar = () => {
+  //get cart items (to find the length of cart items)
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
   return (
     <div className="nav-outer">
       <div className="nav-inner">
@@ -14,7 +19,15 @@ const NavBar = () => {
           <p className="nav-blurb">Homemade ceramic creations</p>
         </div>
         <Link to={"/cart"}>
-          <FontAwesomeIcon icon={faShoppingCart} className="shop-icon icon" />
+          <div className="icon-holder">
+            <FontAwesomeIcon icon={faShoppingCart} className="shop-icon icon" />
+
+            {cartItems.length === 0 ? (
+              <></>
+            ) : (
+              <div className="cart-counter">{cartItems.length}</div>
+            )}
+          </div>
         </Link>
       </div>
     </div>
