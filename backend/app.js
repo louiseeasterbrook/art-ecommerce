88b3783a02cab4eb16a36d48af87c0e9a3f3de1c
louiseeasterbrook <louise.easterbrook@gmail.com>
2021-10-02 00:35:14 +1000

    move connection string to schemas

--- backend/app.js
+++ backend/app.js
@@ -1,29 +1,23 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const mongoose = require("mongoose");
 const productRouter = require("./controllers/productApi");
 const imageRouter = require("./controllers/imagesApi");
+const middleware = require("./utils/middleware.js");
 
 require("dotenv").config();
 
 module.exports = app;
 
-//connect to database
-mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => {
-    console.log("connect to mongo db");
-  })
-  .catch((err) => {
-    console.log(`error connecting to mondoDB: ${err}`);
-  });
-
 app.use(cors());
 app.use(express.json());
+app.use(middleware.requestLogger);
 
 //routes
 app.use("/api/products", productRouter);
 app.use("/api/slideshow", imageRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
 module.exports = app;
--- backend/models/productSchema.js
+++ backend/models/productSchema.js
@@ -1,5 +1,17 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const config = require("../utils/config");
+
+const url = `mongodb+srv://${config.USERNAME}:${config.PASSWORD}@products.bm9h4.mongodb.net/PotteryEcommerce?retryWrites=true&w=majority`;
+//connect to database
+mongoose
+  .connect(url)
+  .then(() => {
+    console.log("connect to mongo db");
+  })
+  .catch((err) => {
+    console.log(`error connecting to mondoDB: ${err}`);
+  });
 
 const productSchema = new Schema({
   name: {
