81f147cdd9be425b9f6cb21d215cd8415b32f2ae
louiseeasterbrook <louise.easterbrook@gmail.com>
2021-10-13 19:57:40 +1100

    load slideshow images from redux

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,17 +1,53 @@
 import "./HomeScreen.css";
+import { useEffect } from "react";
+//redux
+import { useSelector, useDispatch } from "react-redux";
+//actions
+import { getSlideShow } from "../redux/actions/slideShowActions";
 
 //components
-
-import SlideShow from "../components/SlideShow";
+import Carousel, { CarouselItem } from "../components/Carousel";
+import Loading from "../components/Loading";
 
 //sections
 import Section1 from "../sections/Section1";
 import Section2 from "../sections/Section2";
 
 const HomeScreen = () => {
+  const dispatch = useDispatch();
+
+  const getImages = useSelector((state) => state.getSSImages);
+
+  const { images, loading1, error1 } = getImages;
+
+  useEffect(() => {
+    dispatch(getSlideShow());
+  }, [dispatch]);
+
+  // console.log("images from redux= ", getImages.images[0].url);
+
   return (
     <div>
-      <SlideShow />
+      {loading1 ? (
+        <Loading />
+      ) : error1 ? (
+        <h2>{error1}</h2>
+      ) : (
+        <>
+          {console.log(getImages.images[0].url)}
+          {images.map((el, i) => (
+            <p key={i}>{el.url}</p>
+          ))}
+        </>
+        // <Carousel>
+        //   // <CarouselItem>{images[0].url}</CarouselItem>
+        //   // <CarouselItem>{images[1].url}</CarouselItem>
+        //   // <CarouselItem>{images[2].url}</CarouselItem>
+        //   // <CarouselItem>{images[3].url}</CarouselItem>
+        //   // <CarouselItem>{images[0].url}</CarouselItem>
+
+        // </Carousel>
+      )}
       <Section1 />
       <Section2 />
     </div>
