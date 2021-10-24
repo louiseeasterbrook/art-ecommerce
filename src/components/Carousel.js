96fc823fe32a1de6747488b9c8082321b6f3589d
louiseeasterbrook <louise.easterbrook@gmail.com>
2021-10-25 01:23:02 +1100

    update carousel to image slide

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -11,10 +11,10 @@ export const CarouselItem = ({ children, width }) => {
 
 const Carousel = ({ children, width }) => {
   const [activeIndex, setActiveIndex] = useState(0);
-  const [move, setMove] = useState("transform 0.3s");
+  const [move, setMove] = useState("transform 0.5s");
 
   const updateIndex = (newIndex) => {
-    setMove("transform 0.3s");
+    setMove("transform 0.5s");
     if (newIndex < 0) {
       newIndex = React.Children.count(children) - 1;
     } else if (newIndex >= React.Children.count(children)) {
@@ -24,19 +24,19 @@ const Carousel = ({ children, width }) => {
     setActiveIndex(newIndex);
     //if at last image loop back to first image with no transition
     if (newIndex === React.Children.count(children) - 1) {
-      console.log("here");
+      //set index back to 0 (first image) after .5s sliding transition
       setTimeout(() => {
-        console.log("second");
+        //set css transition to none to return to first image
         setMove("none");
         setActiveIndex(0);
-      }, 400);
+      }, 500);
     }
   };
 
   useEffect(() => {
     const interval = setInterval(() => {
       updateIndex(activeIndex + 1);
-    }, 3000);
+    }, 6000);
     return () => {
       if (interval) {
         clearInterval(interval);
@@ -46,44 +46,33 @@ const Carousel = ({ children, width }) => {
 
   return (
     <div className="carousel">
-      <div
-        className="inner"
-        style={{
-          transform: `translateX(-${activeIndex * 100}%`,
-          transition: `${move}`,
-        }}
-      >
-        {React.Children.map(children, (child, index) => {
-          return React.cloneElement(child, { width: "100%" });
-        })}
-      </div>
-      <div className="indicators">
-        <button
-          onClick={() => {
-            updateIndex(activeIndex - 1);
+      <div className="center-img">
+        <div
+          className="inner"
+          style={{
+            transform: `translateX(-${activeIndex * 100}vw`,
+            transition: `${move}`,
           }}
         >
-          Prev
-        </button>
+          {React.Children.map(children, (child, index) => {
+            return React.cloneElement(child, { width: "100vw" });
+          })}
+        </div>
+      </div>
+      <div className="indicators">
         {React.Children.map(children, (child, index) => {
+          if (index === React.Children.count(children) - 1) {
+            return;
+          }
           return (
-            <button
+            <div
               className={`${index === activeIndex ? "active" : ""}`}
               onClick={() => {
                 updateIndex(index);
               }}
-            >
-              {index + 1}
-            </button>
+            ></div>
           );
         })}
-        <button
-          onClick={() => {
-            updateIndex(activeIndex + 1);
-          }}
-        >
-          Next
-        </button>
       </div>
     </div>
   );
