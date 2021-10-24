00d653154205ade7a43f533c1e883c0e2f2e0e4b
louiseeasterbrook <louise.easterbrook@gmail.com>
2021-10-25 01:22:08 +1100

    remove redux import of images

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,53 +1,29 @@
 import "./HomeScreen.css";
-import { useEffect } from "react";
-//redux
-import { useSelector, useDispatch } from "react-redux";
-//actions
-import { getSlideShow } from "../redux/actions/slideShowActions";
 
 //components
 import Carousel, { CarouselItem } from "../components/Carousel";
-import Loading from "../components/Loading";
 
 //sections
 import Section1 from "../sections/Section1";
 import Section2 from "../sections/Section2";
 
+//slideshow images
+import ss1 from "../images/ss1.jpeg";
+import ss2 from "../images/ss2.jpeg";
+import ss3 from "../images/ss3.jpeg";
+import ss4 from "../images/ss4.jpeg";
+
 const HomeScreen = () => {
-  const dispatch = useDispatch();
-
-  const getImages = useSelector((state) => state.getSSImages);
-
-  const { images, loading1, error1 } = getImages;
-
-  useEffect(() => {
-    dispatch(getSlideShow());
-  }, [dispatch]);
-
-  // console.log("images from redux= ", getImages.images[0].url);
-
   return (
     <div>
-      {loading1 ? (
-        <Loading />
-      ) : error1 ? (
-        <h2>{error1}</h2>
-      ) : (
-        <>
-          {console.log(getImages.images[0].url)}
-          {images.map((el, i) => (
-            <p key={i}>{el.url}</p>
-          ))}
-        </>
-        // <Carousel>
-        //   // <CarouselItem>{images[0].url}</CarouselItem>
-        //   // <CarouselItem>{images[1].url}</CarouselItem>
-        //   // <CarouselItem>{images[2].url}</CarouselItem>
-        //   // <CarouselItem>{images[3].url}</CarouselItem>
-        //   // <CarouselItem>{images[0].url}</CarouselItem>
+      <Carousel>
+        <CarouselItem>{ss1}</CarouselItem>
+        <CarouselItem>{ss2}</CarouselItem>
+        <CarouselItem>{ss3}</CarouselItem>
+        <CarouselItem>{ss4}</CarouselItem>
+        <CarouselItem>{ss1}</CarouselItem>
+      </Carousel>
 
-        // </Carousel>
-      )}
       <Section1 />
       <Section2 />
     </div>
